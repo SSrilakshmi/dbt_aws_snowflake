7243b4765de4ae47161c052767fed46b8b5678ae
SSrilakshmi <sagirajusri@gmail.com>
2026-05-09 16:22:05 -0400

    incremental modal complete

--- dbt_aws_snowflake_project/models/bronze/bronze_listings.sql
+++ dbt_aws_snowflake_project/models/bronze/bronze_listings.sql
@@ -1,2 +1,11 @@
+{{
+  config(
+    materialized = 'incremental'
+    )
+}}
+
 select *
 from {{ source('staging', 'listings') }}
+{% if is_incremental() %}
+where created_at > (select coalesce(max(created_at), '1900-01-01') from {{ this }})
+{% endif %}
